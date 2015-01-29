b08b732ef67b6131737580bfa606aff610fd8508
Ralph <ralphxiaoz@gmail.com>
2015-01-29 00:29:53 -0500

    improvement v3

--- movie_data_P2_reborn.rb
+++ movie_data_P2_reborn.rb
@@ -1,31 +1,13 @@
-require 'csv'
+require "#{File.dirname(__FILE__)}/Aux.rb"
 
 class MovieData
 	
 	def initialize(dir, indicator)
 		@dir = dir
 		@indicator = indicator
-		@um_hsh = {} # user_movie hash: Key - user, Value - movie
-		@mu_hsh = {} # movie_user hash: Key - movie, Value - user
 		base_file = File.dirname(__FILE__) + "/#{@dir}/#{@indicator}.base"
-
-		load_file(base_file)
-		
-	end
-
-	def load_file(file_path)
-		CSV.foreach(file_path, {:col_sep => "\t"}) do |line|
-			if @um_hsh.has_key?(line[0])
-				@um_hsh[line[0]][line[1]] = [line[2], line[3]]
-			else
-				@um_hsh[line[0]] = {line[1] => [line[2], line[3]]}
-			end
-			if @mu_hsh.has_key?(line[1])
-				@mu_hsh[line[1]][line[0]] = [line[2], line[3]]
-			else
-				@mu_hsh[line[1]] = {line[0] => [line[2], line[3]]}
-			end
-		end
+		@base_um_hsh = Aux.load_file(base_file, "um")
+		@base_mu_hsh = Aux.load_file(base_file, "mu")
 	end
 
 	# Return a number that indicates the popularity (higher numbers are more popular). 
@@ -34,17 +16,17 @@ class MovieData
 	def popularity(movie_id)
 	  total_rate = 0
 	  times = []
+	  days = 0
 
-	  @mu_hsh[movie_id].each do |u, v|
+	  @base_mu_hsh[movie_id].each do |u, v|
 	  	total_rate += v[0].to_i
 	  	times.push(v[1].to_i)
 	  end
 
 	  times.sort!
-	  days_between = (times.last - times.first) / (3600 * 24)
-	  
-	  if days_between != 0
-	    return (total_rate.to_f / days_between.to_f).round(2)
+	  days = ((times.last - times.first) / (3600 * 24)).to_f
+	  if days != 0
+	    return (total_rate.to_f / days).round(2)
 	  else
 	    return 0
 	  end
@@ -57,7 +39,7 @@ class MovieData
 	  
 	  f = open(pop_file_path, "w")
 
-	  @mu_hsh.keys.each do |m|
+	  @base_mu_hsh.keys.each do |m|
 	  	pop_hsh[m] = popularity(m)
 	  end
 
@@ -71,22 +53,20 @@ class MovieData
 	  return pop_hsh_dec
 	end
 
-	# calculate similarity of 2 hashes
+	# calculate similarity of 2 hashed using cosine similarity
 	def hash_sim(hsh1, hsh2)
-		modified_hsh1 = hsh1.select{|k,v| hsh2.has_key?(k)}
-		modified_hsh2 = hsh2.select{|k,v| hsh1.has_key?(k)}
-		print "hsh1: ", modified_hsh1, "\n"
-		print "hsh2: ", modified_hsh2, "\n"
-		if modified_hsh1.size < 3
+		common_keys = hsh1.keys & hsh2.keys
+
+		if common_keys.size < 3
 			return 0
 		else
 			sum = 0
 			power_sum_hsh1 = 0
 			power_sum_hsh2 = 0
-			modified_hsh1.each do |k,v|
-				sum += modified_hsh1[k] * modified_hsh2[k]
-				power_sum_hsh1 += modified_hsh1[k] **2
-				power_sum_hsh2 += modified_hsh2[k] **2
+			common_keys.each do |k|
+				sum += hsh1[k] * hsh2[k]
+				power_sum_hsh1 += hsh1[k] **2
+				power_sum_hsh2 += hsh2[k] **2
 			end
 			return (sum.to_f / Math.sqrt(power_sum_hsh1 * power_sum_hsh2) * 100).round(3)
 		end
@@ -100,10 +80,10 @@ class MovieData
 	  user1_hsh = Hash.new
 	  user2_hsh = Hash.new
 
-	  @um_hsh[user1].each do |k, v|
+	  @base_um_hsh[user1].each do |k, v|
 	  	user1_hsh[k] = v[0].to_i
 	  end
-	  @um_hsh[user2].each do |k, v|
+	  @base_um_hsh[user2].each do |k, v|
 	  	user2_hsh[k] = v[0].to_i
 	  end
 
@@ -114,7 +94,7 @@ class MovieData
 	# The result will be saved to file "./similarity_list"
 	def most_similar(u, save)
 
-	  user_list = @um_hsh.keys
+	  user_list = @base_um_hsh.keys
 	  user_list.delete(u)
 	  sim_hash = Hash.new
 	  
@@ -142,8 +122,8 @@ class MovieData
 
 	# return the rating of a user on a certain movie
 	def rating(u ,m)
-		if @um_hsh[u].has_key?(m)
-			return @um_hsh[u][m][0]	
+		if @base_um_hsh[u].has_key?(m)
+			return @base_um_hsh[u][m][0]	
 		else
 			return nil
 		end
@@ -177,28 +157,29 @@ class MovieData
 			end
 		end
 		
-		prediction = array_mean(ratings)
-		return prediction.round(2)
-	end
-
-	def array_mean(arr)
-		return arr.inject{ |sum, el| sum + el }.to_f / arr.size
+		prediction = Aux.array_mean(ratings)
+		if prediction.nan?
+			return 3
+		else
+			return prediction
+		end
+		
 	end
 
 	# returns the array of movies that user u has watched
 	def movies(u)
-		return @um_hsh[u].keys
+		return @base_um_hsh[u].keys
 	end
 
 	# returns the array of users that have seen movie m
 	def viewers(m)
-		return @mu_hsh[m].keys
+		return @base_mu_hsh[m].keys
 	end
 
 	# run the test
-	def run_test
+	def run_test(rows = nil)
 
-		mt = MovieTest.new(@dir, @indicator)
+		mt = MovieTest.new(@dir, @indicator, rows)
 
 		mt.test
 		puts "Mean: #{mt.mean}"
@@ -209,57 +190,35 @@ end
 
 class MovieTest
 	
-	def initialize(dir, indicator)
+	def initialize(dir, indicator, rows = nil)
 		@tf_lines = []
+		@error_mean = 0
+		@errors = 0
 		test_file = File.dirname(__FILE__) + "/#{dir}/#{indicator}.test"
-	
-		tf = open(test_file)
-		tf_content = tf.read.split("\n")
-		tf_content.each do |line|
-			@tf_lines.push(line.split("\t"))
-		end
-		puts "Test file loaded."
-		tf.close
-
+		@test_um_hsh = Aux.load_file(test_file, "um", rows)
 		@md_test = MovieData.new(dir, indicator)
+		@result = []
 		puts "Base file for testing loaded."
-
-		@error_mean
-
 	end
 
 	# Predicting the rating of a user in test set based on the base set.
 	# For each iterate, will have to go over the whole base file at least once.
 	# Result stored into file ~/result
 	def test
+		sim_users = []
+		time_start = 0
+		time_end = 0
+		print "Test started at #{time_start = Time.now}. This might take a while.\n"
 
-		if File.exist?(File.dirname(__FILE__) + "/result")
-			return
-		else
-			sim_users = []
-			predicted_users = []
-			f = open(File.dirname(__FILE__) + "/result", "w")
-			f.truncate(0)
-			f.close
-			time_start = 0
-			time_end = 0
-			puts "Test started at #{time_start = Time.now}"
-			print "Test running."
-			@tf_lines.each_with_index do |line, index|
-				f = open(File.dirname(__FILE__) + "/result", "a")
-				if !predicted_users.include?(line[0])
-					# puts Time.now
-					sim_users = @md_test.top_sim_user(line[0])
-					predicted_users.push(line[0])
-				end
-				prediction = @md_test.predict(line[0], line[1], sim_users)
+		@test_um_hsh.each do |u, mhash|
+			sim_users = @md_test.top_sim_user(u)
+			mhash.each do |m, v|
 				print "."
-				f.write("#{line[0]}\t#{line[1]}\t#{line[2]}\t#{prediction}\n")
-				f.close
+				@result.push([u, m, v[0], @md_test.predict(u, m, sim_users)])
 			end
-			puts "Test termintated at #{time_end = Time.now}. Took #{((time_end - time_start).to_f/60).round(2)} minutes."
-			puts "Result stored in #{__FILE__}/result"
 		end
+		puts "\nTest termintated at #{time_end = Time.now}."
+		puts "Took #{((time_end - time_start).to_f/60).round(2)} minutes."
 	end
 
 	# load_result loads the test result file generated from test.
@@ -278,53 +237,35 @@ class MovieTest
 
 	# returns the average predication error (which should be close to zero)
 	def mean
-		error_sum = 0
-		count = 0
-		test_result = MovieTest.load_result
-		test_result.each do |line|
-			if line[3] != "NaN"
-				error_sum += (line[2].to_f - line[3].to_f).abs
-				count += 1
-			end
+		@errors = []
+		@result.each do |r|
+			@errors.push((r[2].to_f - r[3].to_f).abs)
 		end
-		@error_mean = (error_sum / count).round(2)
+		@error_mean = Aux.array_mean(@errors)
 		return @error_mean
 	end
 
 	# returns the standard deviation of the error
 	def stddev
-		sum = 0
-		count = 0
-		test_result = MovieTest.load_result
-		test_result.each do |line|
-			if line[3] != "NaN"
-				sum += ((line[3].to_f - line[2].to_f).abs - @error_mean) **2
-				count += 1
-			end
+		dev = []
+		@result.each do |r|
+			dev.push(((r[2].to_f - r[3].to_f).abs - @error_mean) **2)
 		end
-		stddev = Math.sqrt(sum/count).round(2)
-		return stddev
+		return Math.sqrt(Aux.array_mean(dev)).round(2)
 	end
 
 	# returns the root mean square error of the prediction
 	def rms
-		sum = 0
-		count = 0
-		test_result = MovieTest.load_result
-		test_result.each do |line|
-			if line[3] != "NaN"
-				sum += (line[3].to_f - line[2].to_f) **2
-				count += 1
-			end
+		error_power = []
+		@errors.each do |err|
+			error_power.push(err **2)
 		end
-		rms = Math.sqrt(sum/count).round(2)
-		return rms
+		return Aux.array_mean(error_power)
 	end
 
 	# returns an array of the predictions in the form [u,m,r,p].
 	def to_a
-		prediction_array = MovieTest.load_result
-		return prediction_array
+		return @result
 	end
 end
 
@@ -351,7 +292,7 @@ loop do
   when "1"
     print "Input movie id: "
     movie_id = $stdin.gets.chomp
-    print "The popularity of movie##{movie_id} is ", md.popularity(movie_id), "\n"
+    print "The popularity of movie##{movie_id} is #{md.popularity(movie_id)}\n"
   when "2"
     md.popularity_list
   when "3"
@@ -360,7 +301,7 @@ loop do
     print "Input second user id: "
     user2_id = $stdin.gets.chomp
     sim = md.similarity(user1_id, user2_id)
-    print "Similarity between user# #{user1_id} and user# #{user2_id} is: #{sim}%\n"
+    puts "Similarity between user# #{user1_id} and user# #{user2_id} is: #{sim}%\n"
   when "4"
     print "Input user id: "
     user_id = $stdin.gets.chomp
@@ -391,7 +332,9 @@ loop do
   	movie_id = $stdin.gets.chomp
   	puts "Users that viewed movie##{movie_id} are: #{md.viewers(movie_id)}"
   when "9"
-  	md.run_test
+  	print "Input number of rows to test(default: all):"
+  	num = Integer(gets) rescue nil
+	md.run_test(num)
   when "0"
     exit(0)
   else
